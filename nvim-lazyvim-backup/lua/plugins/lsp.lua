3811216b23243c1a2558b0c185851d13bae31738
Kyle Nguyen <knguyen1193@outlook.com>
2024-03-19 14:18:41 -0700

    commit first time

---
+++ nvim-lazyvim-backup/lua/plugins/lsp.lua
@@ -0,0 +1,228 @@
+return {
+	-- tools
+	{
+		"williamboman/mason.nvim",
+		opts = function(_, opts)
+			vim.list_extend(opts.ensure_installed, {
+				"luacheck",
+				"shellcheck",
+				"shfmt",
+				"tailwindcss-language-server",
+				"typescript-language-server",
+				"css-lsp",
+				"clangd",
+			})
+		end,
+	},
+
+	-- lsp servers
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			inlay_hints = { enabled = true },
+			---@type lspconfig.options
+			servers = {
+
+				clangd = {
+					keys = {
+						{ "<leader>cR", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+					},
+					root_dir = function(fname)
+						return require("lspconfig.util").root_pattern(
+							"Makefile",
+							"configure.ac",
+							"configure.in",
+							"config.h.in",
+							"meson.build",
+							"meson_options.txt",
+							"build.ninja"
+						)(fname) or require("lspconfig.util").root_pattern(
+							"compile_commands.json",
+							"compile_flags.txt"
+						)(fname) or require("lspconfig.util").find_git_ancestor(fname)
+					end,
+					capabilities = {
+						offsetEncoding = { "utf-16" },
+					},
+					cmd = {
+						"clangd",
+						"--background-index",
+						"--clang-tidy",
+						"--header-insertion=iwyu",
+						"--completion-style=detailed",
+						"--function-arg-placeholders",
+						"--fallback-style=llvm",
+					},
+					init_options = {
+						usePlaceholders = true,
+						completeUnimported = true,
+						clangdFileStatus = true,
+					},
+				},
+
+				cssls = {},
+				tailwindcss = {
+					root_dir = function(...)
+						return require("lspconfig.util").root_pattern(".git")(...)
+					end,
+				},
+				tsserver = {
+					root_dir = function(...)
+						return require("lspconfig.util").root_pattern(".git")(...)
+					end,
+					single_file_support = false,
+					settings = {
+						typescript = {
+							inlayHints = {
+								includeInlayParameterNameHints = "literal",
+								includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+								includeInlayFunctionParameterTypeHints = true,
+								includeInlayVariableTypeHints = false,
+								includeInlayPropertyDeclarationTypeHints = true,
+								includeInlayFunctionLikeReturnTypeHints = true,
+								includeInlayEnumMemberValueHints = true,
+							},
+						},
+						javascript = {
+							inlayHints = {
+								includeInlayParameterNameHints = "all",
+								includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+								includeInlayFunctionParameterTypeHints = true,
+								includeInlayVariableTypeHints = true,
+								includeInlayPropertyDeclarationTypeHints = true,
+								includeInlayFunctionLikeReturnTypeHints = true,
+								includeInlayEnumMemberValueHints = true,
+							},
+						},
+					},
+				},
+				html = {},
+
+				-- clangd = {
+				-- 	cmd = { "clangd", "--background-index", "--suggest-missing-includes" },
+				-- 	filetypes = { "c", "cpp", "objc", "objcpp" },
+				-- 	root_dir = function(fname)
+				-- 		return lspconfig.util.root_pattern("compile_commands.json", ".git")(fname)
+				-- 			or lspconfig.util.path.dirname(fname)
+				-- 	end,
+				-- 	on_attach = function(client, bufnr)
+				-- 		client.server_capabilities.offsetEncoding = { "utf-16" }
+				-- 	end,
+				-- },
+
+				-- clangd = {
+				--         keys = {
+				--           { "<leader>cR", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+				--         },
+				--         root_dir = function(fname)
+				--           return require("lspconfig.util").root_pattern(
+				--             "Makefile",
+				--             "configure.ac",
+				--             "configure.in",
+				--             "config.h.in",
+				--             "meson.build",
+				--             "meson_options.txt",
+				--             "build.ninja"
+				--           )(fname) or require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(
+				--             fname
+				--           ) or require("lspconfig.util").find_git_ancestor(fname)
+				--         end,
+				--         capabilities = {
+				--           offsetEncoding = { "utf-16" },
+				--         },
+				--         cmd = {
+				--           "clangd",
+				--           "--background-index",
+				--           "--clang-tidy",
+				--           "--header-insertion=iwyu",
+				--           "--completion-style=detailed",
+				--           "--function-arg-placeholders",
+				--           "--fallback-style=llvm",
+				--         },
+				--         init_options = {
+				--           usePlaceholders = true,
+				--           completeUnimported = true,
+				--           clangdFileStatus = true,
+				--         },
+				--       },
+				--     },
+				--     },
+
+				lua_ls = {
+					-- enabled = false,
+					single_file_support = true,
+					settings = {
+						Lua = {
+							workspace = {
+								checkThirdParty = false,
+							},
+							completion = {
+								workspaceWord = true,
+								callSnippet = "Both",
+							},
+							misc = {
+								parameters = {
+									-- "--log-level=trace",
+								},
+							},
+							hint = {
+								enable = true,
+								setType = false,
+								paramType = true,
+								paramName = "Disable",
+								semicolon = "Disable",
+								arrayIndex = "Disable",
+							},
+							doc = {
+								privateName = { "^_" },
+							},
+							type = {
+								castNumberToInteger = true,
+							},
+							diagnostics = {
+								disable = { "incomplete-signature-doc", "trailing-space" },
+								-- enable = false,
+								groupSeverity = {
+									strong = "Warning",
+									strict = "Warning",
+								},
+								groupFileStatus = {
+									["ambiguity"] = "Opened",
+									["await"] = "Opened",
+									["codestyle"] = "None",
+									["duplicate"] = "Opened",
+									["global"] = "Opened",
+									["luadoc"] = "Opened",
+									["redefined"] = "Opened",
+									["strict"] = "Opened",
+									["strong"] = "Opened",
+									["type-check"] = "Opened",
+									["unbalanced"] = "Opened",
+									["unused"] = "Opened",
+								},
+								unusedLocalExclude = { "_*" },
+							},
+							format = {
+								enable = false,
+								defaultConfig = {
+									indent_style = "space",
+									indent_size = "2",
+									continuation_indent_size = "2",
+								},
+							},
+						},
+					},
+				},
+			},
+			setup = {},
+		},
+	},
+	{
+		"nvim-cmp",
+		dependencies = { "hrsh7th/cmp-emoji" },
+		opts = function(_, opts)
+			table.insert(opts.sources, { name = "emoji" })
+			table.insert(opts.sorting.comparators, 1, require("clangd_extensions.cmp_scores"))
+		end,
+	},
+}
